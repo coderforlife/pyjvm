3fd9f01d494f08960a4aabd9461dfe78ede3febd
Jeffrey Bush <jeff@coderforlfie.com>
2018-06-05 16:15:36 -0400

    Added document header to synth.pyx

--- jvm/internal/synth.pyx
+++ jvm/internal/synth.pyx
@@ -1,3 +1,34 @@
+"""
+JVM internal Python module. While most of the code is in Cython, this module includes functions for
+basic control of the JVM and Java pseudo-modules.
+
+pyjvm - Java VM access from within Python
+Copyright (C) 2016 Jeffrey Bush <jeff@coderforlife.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Synthetic Java Classes
+----------------------
+Create Java classes dynamically that map to Python.
+
+Internal functions:
+    synth_class - creates a synthetic Java class
+    
+TODO:
+    make system for better defining synthetic classes
+"""
+
 include "version.pxi"
 
 from libc.stdlib cimport malloc, free
